e4d32a0f090ac8d607873591cb73d36b9d9e30ab
Evgeniia1707 <42549874+Evgeniia1707@users.noreply.github.com>
2018-10-16 17:16:41 +0300

    Update sport.js

--- sport.js
+++ sport.js
@@ -1,6 +1,6 @@
 function ab() {
 var a = document.sport.sp.value
-if (a ==127) {
+if (a ==126) {
   alert( 'Так, будемо рухатись далі!' );
 } else {
   alert( 'Ні, спробуй ще раз!' ); // любое значение, кроме 2011
