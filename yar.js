1450d9cc79d237b6128f1f5b9442ca54f13a9be4
Evgeniia1707 <42549874+Evgeniia1707@users.noreply.github.com>
2018-10-16 17:23:24 +0300

    Update yar.js

--- yar.js
+++ yar.js
@@ -1,7 +1,7 @@
 function ab() {
 var a = document.yar.yar1.value;
   b = document.yar.yar2.value
-if (a==23.9 && b==31.5) {
+if (a==22.9 && b==31.5) {
   alert( 'Так, будемо рухатись далі!' );
 } else {
   alert( 'Ні, спробуй ще раз!' ); 
